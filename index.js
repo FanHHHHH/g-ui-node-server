61df6305c399b6d9fd2786009158c072ea1f002f
hef <hefan813@vip.qq.com>
2021-01-27 21:37:53 +0800

    init

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const multer = require('multer')
+var cors = require('cors')
+const upload = multer({ dest: 'uploads/' })
+
+const app = express()
+
+app.options('/upload', cors())
+
+app.post('/upload', cors(), upload.single('file'), (req, res) => {
+  res.send(req.file.filename)
+})
+app.get('/preview/:key', cors(), (req, res) => {
+  // 遍历获取文件
+  res.sendFile(
+    `uploads/${req.params.key}`,
+    {
+      root: __dirname,
+      headers: {
+        'Content-Type': 'images/jpeg',
+      },
+    },
+    (error) => {
+      console.log(error)
+      //   res.status(404).send('file not fount')
+    }
+  )
+})
+var port = process.env.PORT || 3000
+app.listen(port, () => {
+  console.log('监听端口：' + port)
+})
